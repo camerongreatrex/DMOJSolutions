a1678c58f8019d56fad3ac627befe4a446d29753
camerongreatrex <147841281+camerongreatrex@users.noreply.github.com>
2023-10-13 11:17:24 -0400

    added all current DMOJ files

---
+++ AlpacaShapes.java
@@ -0,0 +1,22 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AlpacaShapes {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num[] = new int[2];
+        String[] strNums;
+        strNums = br.readLine().split(" ");
+        for (int i = 0; i < strNums.length; i++) {
+            num[i] = Integer.parseInt(strNums[i]);
+        }
+        if (Math.pow(num[0], 2) > 3.14 * Math.pow(num[1], 2)) {
+            System.out.println("SQUARE");
+        } else if (Math.pow(num[0], 2) < 3.14 * Math.pow(num[1], 2)) {
+            System.out.println("CIRCLE");
+        }
+    }
+}
---
+++ Koala.java
@@ -0,0 +1,13 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Koala {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Long.parseLong(br.readLine())-1);
+    }
+}
