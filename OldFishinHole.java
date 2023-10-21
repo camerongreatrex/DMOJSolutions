c641acbe6fc125ae3ddf68e10f80a337efd09cf0
camerongreatrex <147841281+camerongreatrex@users.noreply.github.com>
2023-10-20 21:03:25 -0400

    revision 2 final to find combos for each fish seperately

--- OldFishinHole.java
+++ OldFishinHole.java
@@ -17,23 +17,37 @@ public class OldFishinHole {
         byte pickerelCount = 0;
         short maxPoints = Short.parseShort(br.readLine());
         short possiblityCount = 0;
-        for(int i = 1; i <= maxPoints; i++) {
-            if(troutValue*i <= maxPoints) {
-                possiblityCount++;
-                troutCount++;
-                System.out.println(troutCount + " Brown Trout, 0 Northern Pike, 0 Yellow Pickerel");
-            }
-            if(pikeValue*i <= maxPoints) {
-                possiblityCount++;
-                pikeCount++;
-                System.out.println("0 Brown Trout, " + pikeCount + " Northern Pike, 0  Yellow Pickerel");
-            }
-            if(pickerelValue*i <= maxPoints) {
-                possiblityCount++;
-                pickerelCount++;
-                System.out.println("0 Brown Trout, 0 Northern Pike, " + pickerelCount + " Yellow Pickerel");
-            }
+        //trout points if only trouts
+        for (int i = troutValue, k = 1; i <= maxPoints; i += troutValue, k++) {
+            possiblityCount++;
+            System.out.println(k + " Brown Trout, 0 Northern Pike, 0 Yellow Pickerel");
         }
+        //pike points if only pikes
+        for (int i = pikeValue, k = 1; i <= maxPoints; i += pikeValue, k++) {
+            possiblityCount++;
+            System.out.println("0 Brown trout, " + k + " Northern Pike, 0 Yellow Pickerel");
+        }
+        //pickerel points if only pikes
+        for (int i = pickerelValue, k = 1; i <= maxPoints; i += pickerelValue, k++) {
+            possiblityCount++;
+            System.out.println("0 Brown trout, 0 Northern Pike, " + k + " Yellow Pickerel");
+        }
+
+//            if(troutValue*i <= maxPoints) {
+//                possiblityCount++;
+//                troutCount++;
+//                System.out.println(troutCount + " Brown Trout, 0 Northern Pike, 0 Yellow Pickerel");
+//            }
+//            if(pikeValue*i <= maxPoints) {
+//                possiblityCount++;
+//                pikeCount++;
+//                System.out.println("0 Brown Trout, " + pikeCount + " Northern Pike, 0  Yellow Pickerel");
+//            }
+//            if(pickerelValue*i <= maxPoints) {
+//                possiblityCount++;
+//                pickerelCount++;
+//                System.out.println("0 Brown Trout, 0 Northern Pike, " + pickerelCount + " Yellow Pickerel");
+//            }
         System.out.println("Number of ways to catch fish: " + possiblityCount);
     }
 }
