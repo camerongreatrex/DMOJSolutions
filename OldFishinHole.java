a24f905adc295606fdd2ee3cdd92110b79b16456
camerongreatrex <147841281+camerongreatrex@users.noreply.github.com>
2023-10-20 21:15:17 -0400

    revision 3 final

--- OldFishinHole.java
+++ OldFishinHole.java
@@ -10,44 +10,39 @@ public class OldFishinHole {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //uses byte and short to minimize memory usage for the inputs
         byte troutValue = Byte.parseByte(br.readLine());
-        byte troutCount = 0;
         byte pikeValue = Byte.parseByte(br.readLine());
-        byte pikeCount = 0;
         byte pickerelValue = Byte.parseByte(br.readLine());
-        byte pickerelCount = 0;
         short maxPoints = Short.parseShort(br.readLine());
-        short possiblityCount = 0;
-        //trout points if only trouts
-        for (int i = troutValue, k = 1; i <= maxPoints; i += troutValue, k++) {
-            possiblityCount++;
-            System.out.println(k + " Brown Trout, 0 Northern Pike, 0 Yellow Pickerel");
-        }
-        //pike points if only pikes
-        for (int i = pikeValue, k = 1; i <= maxPoints; i += pikeValue, k++) {
-            possiblityCount++;
-            System.out.println("0 Brown trout, " + k + " Northern Pike, 0 Yellow Pickerel");
-        }
-        //pickerel points if only pikes
-        for (int i = pickerelValue, k = 1; i <= maxPoints; i += pickerelValue, k++) {
-            possiblityCount++;
-            System.out.println("0 Brown trout, 0 Northern Pike, " + k + " Yellow Pickerel");
-        }
+        short possibilityCount = 0;
 
-//            if(troutValue*i <= maxPoints) {
-//                possiblityCount++;
-//                troutCount++;
-//                System.out.println(troutCount + " Brown Trout, 0 Northern Pike, 0 Yellow Pickerel");
-//            }
-//            if(pikeValue*i <= maxPoints) {
-//                possiblityCount++;
-//                pikeCount++;
-//                System.out.println("0 Brown Trout, " + pikeCount + " Northern Pike, 0  Yellow Pickerel");
-//            }
-//            if(pickerelValue*i <= maxPoints) {
-//                possiblityCount++;
-//                pickerelCount++;
-//                System.out.println("0 Brown Trout, 0 Northern Pike, " + pickerelCount + " Yellow Pickerel");
-//            }
-        System.out.println("Number of ways to catch fish: " + possiblityCount);
+        for (byte troutCount = 0; troutCount * troutValue <= maxPoints; troutCount++) {
+            for (byte pikeCount = 0; pikeCount * pikeValue <= maxPoints; pikeCount++) {
+                for (byte pickerelCount = 0; pickerelCount * pickerelValue <= maxPoints; pickerelCount++) {
+                    int totalPoints = troutCount * troutValue + pikeCount * pikeValue + pickerelCount * pickerelValue;
+                    if (totalPoints <= maxPoints && (troutCount > 0 || pikeCount > 0 || pickerelCount > 0)) {
+                        possibilityCount++;
+                        System.out.println(troutCount + " Brown Trout, " + pikeCount + " Northern Pike, " + pickerelCount + " Yellow Pickerel");
+                    }
+                }
+            }
+        }
+        System.out.println("Number of ways to catch fish: " + possibilityCount);
     }
 }
+
+//        //trout points if only trouts
+//        for (int i = troutValue, k = 1; i <= maxPoints; i += troutValue, k++) {
+//            possiblityCount++;
+//            System.out.println(k + " Brown Trout, 0 Northern Pike, 0 Yellow Pickerel");
+//        }
+//        //pike points if only pikes
+//        for (int i = pikeValue, k = 1; i <= maxPoints; i += pikeValue, k++) {
+//            possiblityCount++;
+//            System.out.println("0 Brown trout, " + k + " Northern Pike, 0 Yellow Pickerel");
+//        }
+//        //pickerel points if only pikes
+//        for (int i = pickerelValue, k = 1; i <= maxPoints; i += pickerelValue, k++) {
+//            possiblityCount++;
+//            System.out.println("0 Brown trout, 0 Northern Pike, " + k + " Yellow Pickerel");
+//        }
+//        System.out.println("Number of ways to catch fish: " + possiblityCount);
